b09ee8b3f48f61f9fab1dbd24f9f9e01a308f638
Abhinav-1604 <abhinavkurup0406@gmail.com>
2025-12-19 20:44:12 +0530

    First commit before starting esp32 deployment

---
+++ kws_esp32/kws_esp32.ino
@@ -0,0 +1,11 @@
+//main code for esp32 deployment of the trained model
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
