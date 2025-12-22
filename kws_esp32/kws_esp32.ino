9edc65a74ca776d965e817459601ead39ea7d677
Abhinav-1604 <abhinavkurup0406@gmail.com>
2025-12-22 15:51:55 +0530

    Added a bit of tensorflowlite stuff

--- kws_esp32/kws_esp32.ino
+++ kws_esp32/kws_esp32.ino
@@ -1,4 +1,20 @@
-//main code for esp32 deployment of the trained model
+//Main code for ESP32 deployment
+
+#include <driver/i2s.h> //I2S driver library
+#include <LiquidCrystal.h> //LCD library
+//Edge Impulse libraries
+#include "dsp/speechpy/speechpy.hpp"
+#include "dsp/config.hpp"
+
+//TensorFlowLite
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+
+//Our model
+#include "kws_model.h"
 
 //LED Pin 
 #define LED_PIN 4
@@ -16,6 +32,8 @@
 #define LCD_D6 18
 #define LCD_D7 5
 
+LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
 //I2S Processor Selection
 #define I2S_PORT I2S_NUM_0
 
@@ -28,6 +46,23 @@ int16_t audio_buffer[AUDIO_LEN];
 #define NUM_FRAMES 49
 #define NUM_MFCC 13
 float mfcc features[NUM_FRAMES][NUM_MFCC];
+
+static mfcc_config_t mfcc_config = {
+  .num filters = 40,
+  .num_cepstral  = NUM_MFCC,
+  .frame_length  = 480,   // 30 ms
+  .frame_stride  = 320,   // 20 ms
+  .low_frequency = 20,
+  .high_frequency= 8000,
+  .fft_length    = 512,
+  .pre_emphasis  = 0.98,
+  .window_type   = MFCC_WINDOW_HAMMING,
+  .log_scale     = true
+};
+
+//TFLite Setup
+constexpr int kTensorArenaSize = 70*1024;
+uint8_t tensor_arena[kTensorArenaSize];
 
 //I2S Setup
 void SetupI2S(){
