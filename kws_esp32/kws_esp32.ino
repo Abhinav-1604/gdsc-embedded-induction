46e7feb0212b91e0ab68753c38c010abcf2fd66b
Abhinav-1604 <abhinavkurup0406@gmail.com>
2025-12-24 11:19:01 +0530

    Wrote all functionalities

--- kws_esp32/kws_esp32.ino
+++ kws_esp32/kws_esp32.ino
@@ -5,9 +5,10 @@
 //Edge Impulse libraries
 #include "dsp/speechpy/speechpy.hpp"
 #include "dsp/config.hpp"
+using namespace ei::dsp::speechpy;
 
 //TensorFlowLite
-#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
@@ -72,10 +73,15 @@ namespace{
   TfLiteTensor* model_output = nullptr;
 }
 
+//Error Reporter
+static tflite::MicroErrorReporter micro_error_reporter;
+error_reporter = &micro_error_reporter;
+
 //Class Labels
 #define ON 0
 #define OFF 1
 #define UNKNOWN 2
+int last_result = -1;
 
 //I2S Setup
 void SetupI2S(){
@@ -99,6 +105,106 @@ void SetupI2S(){
   };
   i2s_set_pin(I2S_PORT, &pin_config);
 }
+
+//Recording Audio
+void recordAudio(){
+  size_t bytes_read;
+  int i = 0;
+  while(i<AUDIO_LEN){
+    i2s_read(I2S_PORT, &audio_buffer[i], (AUDIO_LEN - i)*sizeof(int16_t), &bytes_read, portMAX_DELAY);
+    i+=(bytes_read/sizeof(int16_t));
+  }
+}
+
+//Plotting sound from microphone on serial plotter
+void plotAudio(){
+  for(int i=0;i<AUDIO_LEN;i+=16){
+    Serial.println(audio_buffer[i]);
+  }
+}
+
+//Extracting MFCC from recorded audio
+void computeMFCC(){
+static float audio_float[AUDIO_LEN];
+for(int i=0;i<AUDIO_LEN;i++){
+  audio_float[i] = audio_buffer[i]/32768.0f;
+}
+extract_mfcc_features(audio_float, AUDIO_LEN, SAMPLE_RATE, &mfcc_config, mfcc_features);
+}
+
+//Setting up the model
+void setupModel(){
+  model = tflite::GetModel(kws_model_tflite);
+  if(model->version() != TFLITE_SCHEMA_VERSION){
+    error_reporter->Report("Your model version %d does not match Schema version %d", model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+  static tflite::MicroMutableOpResolver<8> resolver;
+  resolver.AddConv2D();
+  resolver.AddDepthwiseConv2D();
+  resolver.AddFullyConnected();
+  resolver.AddSoftmax();
+  resolver.AddReshape();
+  resolver.AddQuantize();
+  resolver.AddDequantize();
+
+  interpreter = new tflite::MicroInterpreter(model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if(allocate_status != kTfLiteOk){
+    error_reporter->Report("AllocateTensors() failed");
+    return;
+  }
+
+  input = interpreter->input(0);
+  output = interpreter->output(0);
+}
+
+//Converting float to int8 and feeding it to input tensors
+void fillInputTensors(){
+  float scale = input->params.scale;
+  int zero_point = input->params.zero_point;
+  for(int i=0; i< (NUM_MFCC*NUM_FRAMES); i++){
+    int8_t q = (int8_t)((mfcc_features[i]/scale) + zero_point);
+    input->data.int8[i] = q;
+  }
+}
+
+//Give input to the model and get output
+void runModel(){
+  interpreter->Invoke();
+  int best = 0;
+  int8_t max_val = output->data.int8[0];
+  for(int i=1;i<3;i++){
+    if(output->data.int8[i]>max_val){
+      max_val = output->data.int8[i];
+      best = i;
+    }
+  }
+  return best;
+}
+
+//LCD functionality
+void updateLCD(int result){
+  if(result == last_result){
+    return;
+  }
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("STATUS: ");
+lcd.setCursor(0,1);
+if(result == ON){
+  lcd.print("ON");
+}
+else if(result == OFF){
+  lcd.print("OFF");
+}
+else{
+  lcd.print("UNKNOWN AUDIO");
+}
+last_result = result;
+}
+
 void setup() {
   // put your setup code here, to run once:
 
