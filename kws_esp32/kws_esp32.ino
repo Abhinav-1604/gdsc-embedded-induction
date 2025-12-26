4aa05f9fd9d74b617c2c5d9a211d69fb82b40740
Abhinav-1604 <abhinavkurup0406@gmail.com>
2025-12-26 20:33:01 +0530

    Main deployment code

--- kws_esp32/kws_esp32.ino
+++ kws_esp32/kws_esp32.ino
@@ -1,5 +1,4 @@
 //Main code for ESP32 deployment
-
 #include <driver/i2s.h> //I2S driver library
 #include <LiquidCrystal.h> //LCD library
 
@@ -9,108 +8,96 @@
 #include <tensorflow/lite/schema/schema_generated.h>
 #include "tensorflow/lite/micro/micro_log.h"
 #include "tensorflow/compiler/mlir/lite/core/api/error_reporter.h"
+#include "audio_provider.h"
+#include "command_responder.h"
+#include "feature_provider.h"
+#include "recognize_commands.h"
 #include "micro_features_generator.h"
 #include "micro_model_settings.h"
 
 //Our model
-#include "kws_model.h"
+#include "model.h"
 
 //LED Pin 
-#define LED_PIN 13
-
-//Microphone Pins
-#define I2S_WS 25
-#define I2S_SD 33
-#define I2S_SCK 32
+#define LED_PIN 2
+uint8_t led_state = 0;
 
 //LCD Pins
-#define LCD_RS 23
-#define LCD_E 22
-#define LCD_D4 21
-#define LCD_D5 19
-#define LCD_D6 18
-#define LCD_D7 5
+#define LCD_RS 13
+#define LCD_E 32
+#define LCD_D4 14
+#define LCD_D5 27
+#define LCD_D6 26
+#define LCD_D7 33
 
 LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-
-//I2S Processor Selection
-#define I2S_PORT I2S_NUM_0
-
-//Audio Settings
-static int16_t audio_buffer[kAudioSampleFrequency];
-
-//MFCC Features
-static int8_t mfcc_buffer[kFeatureCount][kFeatureSize];
-
-//TFLite Setup
-constexpr int kTensorArenaSize = 60*1024;
-uint8_t tensor_arena[kTensorArenaSize];
 
 namespace{
   const tflite::Model* model = nullptr;
   tflite::MicroInterpreter* interpreter = nullptr;
   TfLiteTensor* input = nullptr;
   TfLiteTensor* output = nullptr;
+  FeatureProvider* feature_provider = nullptr;
+  RecognizeCommands* recognizer = nullptr;
+  int32_t previous_time = 0;
+  constexpr int kTensorArenaSize = 35*1024;
+  uint8_t tensor_arena[kTensorArenaSize];
+  int8_t feature_buffer[kFeatureElementCount];
+  int8_t* model_input_buffer = nullptr;
 }
 
 //Class Labels
-#define ON 0
-#define OFF 1
-#define UNKNOWN 2
+#define ON 2
+#define OFF 3
+#define UNKNOWN 1
 int last_result = -1;
 
-//I2S Setup
-void setupI2S(){
- i2s_config_t i2s_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-    .sample_rate = kAudioSampleFrequency,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = I2S_COMM_FORMAT_I2S,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 8,
-    .dma_buf_len = 512,
-    .use_apll = false,
-  };
-  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
- i2s_pin_config_t pin_config = {
-    .bck_io_num = I2S_SCK,
-    .ws_io_num  = I2S_WS,
-    .data_out_num = -1,
-    .data_in_num  = I2S_SD
-  };
-  i2s_set_pin(I2S_PORT, &pin_config);
+//Setting up audio functionalities
+void setupAudio(){
+ static FeatureProvider static_feature_provider(kFeatureElementCount, feature_buffer);
+ feature_provider = &static_feature_provider;
+ static RecognizeCommands static_recognizer;
+ recognizer = &static_recognizer;
+ previous_time = 0;
 }
 
-//Recording Audio
-void recordAudio(){
-  size_t bytes_read;
-  int i = 0;
-  while(i<kAudioSampleFrequency){
-    i2s_read(I2S_PORT, &audio_buffer[i], (kAudioSampleFrequency - i)*sizeof(int16_t), &bytes_read, portMAX_DELAY);
-    i+=(bytes_read/sizeof(int16_t));
-  }
-}
-
-//Extracting MFCC from recorded audio
+//Computing the MFCC for model input
 void computeMFCC(){
- size_t num_samples;
- GenerateFeatures(audio_buffer, kAudioSampleFrequency, &mfcc_buffer);
+  const int32_t current_time = LatestAudioTimestamp();
+  int how_many_new_slices = 0;
+  TfLiteStatus feature_status = feature_provider->PopulateFeatureData(previous_time, current_time, &how_many_new_slices);
+  if (feature_status != kTfLiteOk) {
+    MicroPrintf( "Feature generation failed");
+    return;
+  }
+  previous_time = current_time;
+  if (how_many_new_slices == 0) {
+    return;
+  }
 }
 
 //Setting up the model
 void setupModel(){
-  model = tflite::GetModel(kws_model_tflite);
-  static tflite::MicroMutableOpResolver<8> resolver;
-  resolver.AddConv2D();
-  resolver.AddDepthwiseConv2D();
-  resolver.AddFullyConnected();
-  resolver.AddSoftmax();
-  resolver.AddReshape();
-  resolver.AddQuantize();
-  resolver.AddDequantize();
+  model = tflite::GetModel(g_model);
+  static tflite::MicroMutableOpResolver<5> resolver;
+  if (resolver.AddDepthwiseConv2D() != kTfLiteOk) {
+    return;
+  }
+  if (resolver.AddConv2D() != kTfLiteOk) {
+    return;
+  }
+  if (resolver.AddFullyConnected() != kTfLiteOk) {
+    return;
+  }
+  if (resolver.AddSoftmax() != kTfLiteOk) {
+    return;
+  }
+  if (resolver.AddReshape() != kTfLiteOk) {
+    return;
+  }
 
-  interpreter = new tflite::MicroInterpreter(model, resolver, tensor_arena, kTensorArenaSize);
+  static tflite::MicroInterpreter static_interpreter(model, resolver, tensor_arena, kTensorArenaSize);
+  interpreter = &static_interpreter;
 
   TfLiteStatus allocate_status = interpreter->AllocateTensors();
   if (allocate_status != kTfLiteOk) {
@@ -120,76 +107,92 @@ void setupModel(){
 
 MicroPrintf("AllocateTensors OK");
 
-  input = interpreter->input(0);
-  output = interpreter->output(0);
+input = interpreter->input(0);
+if ((input->dims->size != 2) || (input->dims->data[0] != 1) || (input->dims->data[1] != (kFeatureCount * kFeatureSize)) || (input->type != kTfLiteInt8)) {
+    MicroPrintf("Bad input tensor parameters in model");
+    return;
+  }
+ model_input_buffer = tflite::GetTensorData<int8_t>(input);
 }
 
 //Giving audio to input tensors
-void fillInputTensors(){
-  for(int i=0; i<kFeatureCount; i++){
-    for(int j=0; j<kFeatureSize; j++){
-     input->data.int8[i] = mfcc_buffer[i][j];
-    }
+void fillInputTensors() {
+ for (int i = 0; i < kFeatureElementCount; i++) {
+    model_input_buffer[i] = feature_buffer[i];
   }
 }
-
 //Give input to the model and get output
-int runModel(){
-  interpreter->Invoke();
-  int best = 0;
-  int8_t max_val = output->data.int8[0];
-  for(int i=1;i<3;i++){
-    if(output->data.int8[i]>max_val){
-      max_val = output->data.int8[i];
-      best = i;
+int runModel() {
+  if (interpreter->Invoke() != kTfLiteOk) {
+    MicroPrintf("Invoke FAILED");
+    return UNKNOWN;
+  }
+  output = interpreter->output(0);
+  float output_scale = output->params.scale;
+  int output_zero_point = output->params.zero_point;
+  int max_idx = 0;
+  float max_result = 0.0;
+  // Dequantize output values and find the max
+  for (int i = 0; i < kCategoryCount; i++) {
+    float current_result = (tflite::GetTensorData<int8_t>(output)[i] - output_zero_point)*output_scale;
+    if (current_result > max_result) {
+      max_result = current_result; // update max result
+      max_idx = i; // update category
     }
   }
-  return best;
-}
-
-//LCD functionality
-void updateLCD(int result){
-  if(result == last_result){
-    return;
+  if (max_result > 0.8f) {
+    MicroPrintf("Detected %7s, score: %.2f", kCategoryLabels[max_idx], static_cast<double>(max_result));
   }
-lcd.clear();
-lcd.setCursor(0,0);
-lcd.print("STATUS: ");
-lcd.setCursor(0,1);
-if(result == ON){
-  lcd.print("LED is ON now!");
-}
-else if(result == OFF){
-  lcd.print("LED is OFF now!");
-}
-else{
-  lcd.print("UNKNOWN AUDIO");
-}
-last_result = result;
+  return max_idx;
 }
 
 void setup() {
  Serial.begin(115200);
  pinMode(LED_PIN, OUTPUT);
- lcd.begin(16,2);
- lcd.print("Keyword Spotting On ESP32");
- setupI2S();
+ setupAudio();
  setupModel();
+ lcd.begin(16,2);
+ lcd.print("KEYWORD SPOTTING");
 }
 
 void loop() {
-  recordAudio();
-  computeMFCC();
-  fillInputTensors();
-  int result = runModel();
-  updateLCD(result);
-  if(result == ON){
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("LED is ON now!");
-  }
-  else{
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("LED is OFF now!");
-  }
+Serial.println("STEP 1: mfcc");
+computeMFCC();
 
+Serial.println("STEP 2: fill");
+fillInputTensors();
+
+Serial.println("STEP 3: invoke");
+int result = runModel();
+
+Serial.println("STEP 4: led");
+  if(result == ON){
+   if(led_state == 0){
+    led_state = 1;
+   }
+   else{
+    led_state = 1;
+   }
+  }
+  else if(result == OFF){
+   if(led_state == 1){
+    led_state = 0;
+   }
+   else{
+    led_state = 0;
+   }
+  }
+  if(led_state == 1){
+    Serial.println("LED is ON!");
+    lcd.setCursor(0,1);
+    lcd.print("LED is ON!");
+  }
+  else if(led_state == 0){
+    Serial.println("LED is OFF!");
+    lcd.setCursor(0,1);
+    lcd.print("LED is OFF!");
+  }
+  digitalWrite(LED_PIN, led_state);
 }
+
+
